b857ccdfd268a3df1f35ff5c7dfca754c2df1af6
darrylgit <darrylsmithup@gmail.com>
2020-07-12 17:22:30 -0400

    Complete Questions project

--- 7-questions/setup/app.js
+++ 7-questions/setup/app.js
@@ -1,3 +1,29 @@
 //using selectors inside the element
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(question => {
+  const btn = question.querySelector('.question-btn');
+
+  btn.addEventListener('click', () => {
+    // Enforce only one question open at a time
+    questions.forEach(item => {
+      if (item !== question) {
+        item.classList.remove('show-text');
+      }
+    });
+
+    // Show text in target question
+    question.classList.toggle('show-text');
+  });
+});
 
 // traversing the dom
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach(button =>
+//   button.addEventListener('click', e => {
+//     const question = e.currentTarget.parentElement.parentElement;
+
+//     question.classList.toggle('show-text');
+//   })
+// );
