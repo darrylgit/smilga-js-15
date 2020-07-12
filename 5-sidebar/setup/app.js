4dfdaf836b7c1eee6a7aa46e7f95eb38e4ddc454
darrylgit <darrylsmithup@gmail.com>
2020-07-12 11:48:38 -0400

    Complete sidebar project

---
+++ 5-sidebar/setup/app.js
@@ -0,0 +1,11 @@
+const toggleBtn = document.querySelector('.sidebar-toggle');
+const closeBtn = document.querySelector('.close-btn');
+const sidebar = document.querySelector('.sidebar');
+
+toggleBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('show-sidebar');
+});
+
+closeBtn.addEventListener('click', () => {
+  sidebar.classList.remove('show-sidebar');
+});
