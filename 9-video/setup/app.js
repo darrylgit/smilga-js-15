3f74a2e95b676bfd14b5caa54baa74178e2f2eb9
darrylgit <darrylsmithup@gmail.com>
2020-07-15 22:13:08 -0400

    Add loading image

--- 9-video/setup/app.js
+++ 9-video/setup/app.js
@@ -14,3 +14,10 @@ btn.addEventListener('click', () => {
     video.play();
   }
 });
+
+// preloader
+const preloader = document.querySelector('.preloader');
+
+window.addEventListener('load', () => {
+  preloader.classList.add('hide-preloader');
+});
