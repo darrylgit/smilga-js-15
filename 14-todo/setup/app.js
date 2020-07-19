c8b35f3d38e51e0ea1bedb34bbec1797fb00b5e3
darrylgit <darrylsmithup@gmail.com>
2020-07-18 20:07:21 -0400

    Add item to list

--- 14-todo/setup/app.js
+++ 14-todo/setup/app.js
@@ -1,11 +1,75 @@
 // ****** SELECT ITEMS **********
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const grocery = document.getElementById('grocery');
+const submitBtn = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
 
 // edit option
+let editElement;
+let editFlag = false;
+let editID = '';
 
 // ****** EVENT LISTENERS **********
+//submit form
+form.addEventListener('submit', addItem);
 
 // ****** FUNCTIONS **********
+// add item
+function addItem(e) {
+  e.preventDefault();
+  const value = grocery.value;
+  const id = new Date().getTime().toString();
 
+  if (value && !editFlag) {
+    const element = document.createElement('article');
+    // add class
+    element.classList.add('grocery-item');
+
+    // add id
+    const attr = document.createAttribute('data-id');
+    attr.value = id;
+    element.setAttributeNode(attr);
+
+    // set element content
+    element.innerHTML = `<p class="title">${value}</p>
+    <div class="btn-container">
+      <button type="button" class="edit-btn">
+        <i class="fas fa-edit"></i>
+      </button>
+      <button type="button" class="delete-btn">
+        <i class="fas fa-trash"></i>
+      </button>
+    </div>`;
+
+    // append child
+    list.appendChild(element);
+
+    // display alert
+    displayAlert('item added', 'success');
+
+    // show container
+    container.classList.add('show-container');
+  } else if (value && editFlag) {
+    console.log('editing');
+  } else {
+    displayAlert('please enter value', 'danger');
+  }
+}
+
+// display alert
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+
+  // remove alert
+  setTimeout(() => {
+    alert.textContent = '';
+    alert.classList.remove(`alert-${action}`);
+  }, 1000);
+}
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
